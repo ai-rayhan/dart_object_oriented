4995b590b700fc18e889fae5f1490bcebc1c1e88
RayhanAhmed <rayhanahmed1127@gmail.com>
2023-08-15 21:35:25 +0600

    init

---
+++ bin/dart_object_oriented.dart
@@ -0,0 +1,5 @@
+import 'package:dart_object_oriented/dart_object_oriented.dart' as dart_object_oriented;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_object_oriented.calculate()}!');
+}
